d7e37f09534d516183f76c9b8dd34d8ae32257c4
Karthikeyan Annamalai <karthik2543@gmail.com>
2025-08-28 15:27:29 +0530

    update app.js

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ app.get("/", (req, res) => {
   res.send("Hello from Node.js running on ECS with EC2!");
 });
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
 });
+
 
