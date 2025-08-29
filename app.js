1225d6461cfd65d5c3cde2bbf76b312eb845cd7a
Karthikeyan Annamalai <karthik2543@gmail.com>
2025-08-29 11:18:46 +0530

    update app.js

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js running on ECS with EC2!");
+  res.send("Hello from Node.js running on ECS (EC2) with GitHub Actions CI/CD!");
 });
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
-
-
